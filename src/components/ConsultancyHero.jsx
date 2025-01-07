ef858e1e912a88be87044a50d204ea2117c06da5
Pranav Rathi <87274086+prathi912@users.noreply.github.com>
2025-01-07 09:41:56 +0530

    push

--- src/components/ConsultancyHero.jsx
+++ src/components/ConsultancyHero.jsx
@@ -7,19 +7,27 @@ const ConsultancyHero = () => {
         <h2>About Us</h2>
         <div className="underlineCollaborationsCarousel"></div>
         <p>
-          Consultancy Center, Parul University is an initiative to provide
-          quality testing and consultancy services to various industries,
-          academia and research organizations across India. The center is
-          involved in various tailor-made research services as per the
-          requirement of industries. We offer a broad range of
-          state-of-the-art analytical instruments under one roof and
-          facilities for accurate, prompt and cost-effective analysis and
-          testing. We have trained and qualified staff for sample
-          preparation, testing and interpretation. The goal of the centre is
-          dedicated towards ensuring the highest form of credibility and
-          quality in our consultancy services.
+        The Consultancy Center at Parul University is a multidisciplinary initiative committed to delivering 
+        high-quality consultancy and testing services to industries, academia, and research organizations 
+        across India. With expertise spanning diverse fields such as Pharmacy, Physiotherapy, Ayurveda, 
+        Social work, Commerce, Engineering, Management, IT Solutions, and many more, we cater to a wide 
+        range of needs through customized and innovative solutions.
         </p>
-      </div>
+        <br>
+        </br>
+        <p>Our center is equipped with state-of-the-art facilities and cutting-edge technology, supported by 
+          highly skilled professionals who ensure precision, reliability, and efficiency in every service we provide.
+           From drug formulation and quality testing to financial advisory, market research, technical problem-solving,
+            and IT-based solutions,  we strive to offer cost-effective and prompt results that meet the highest standards
+             of credibility and excellence.  </p>
+             <br>
+             </br>
+             <p>
+             At the Consultancy Center, we are dedicated to fostering collaboration, 
+             driving innovation, and empowering industries and organizations with actionable insights and solutions 
+             to achieve their goals.
+             </p>
+          </div>
       <div className="about-us-image">
         <Image
           src="/AboutBanner.png"
